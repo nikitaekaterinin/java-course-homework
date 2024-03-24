9810232bd923fda95bd183515396d8b81b314acb
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-03-24 18:49:07 +0200

    Added solutions for homework tasks (homework 3)

---
+++ src/main/java/org/example/homework3/MinMax.java
@@ -0,0 +1,16 @@
+package org.example.homework3;
+
+public class MinMax {
+    public static void main(String[] args) {
+        int a = 77;
+        int b = 676;
+
+        int min = (a < b) ? a : b;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Min value is: " + min);
+        System.out.println("Max value is: " + max);
+
+    }
+}

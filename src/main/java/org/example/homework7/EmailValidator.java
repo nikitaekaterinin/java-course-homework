f518118e437555a7cec8e9f0670673aee77bd014
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-04-27 20:03:50 +0300

    Added homework for lection 6

---
+++ src/main/java/org/example/homework7/EmailValidator.java
@@ -0,0 +1,17 @@
+package org.example.homework7;
+
+import java.util.Scanner;
+
+public class EmailValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter email to check: ");
+        String userEmail = sc.nextLine();
+        validator(userEmail);
+    }
+
+    public static void validator(String email) {
+        
+        }
+    }
+

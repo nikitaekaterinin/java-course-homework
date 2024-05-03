ef5224a78c410056490b93707ec7a0779d0a1b68
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-05-03 17:22:27 +0300

    added homework #8

---
+++ src/main/java/org/example/homework8/Bedroom/Wall.java
@@ -0,0 +1,13 @@
+package org.example.homework8.Bedroom;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}

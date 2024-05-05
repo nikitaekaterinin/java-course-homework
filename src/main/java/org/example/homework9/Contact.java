d34e64e887aa0afee2dd30e00dc9ae49b7c2248c
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-05-05 19:41:31 +0300

    added homework #9

---
+++ src/main/java/org/example/homework9/Contact.java
@@ -0,0 +1,42 @@
+package org.example.homework9;
+
+import java.util.Objects;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Contact contact = (Contact) obj;
+        return Objects.equals(name, contact.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}

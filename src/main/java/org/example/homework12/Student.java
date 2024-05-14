f83f9d0d606c97155b1ace182255007a1c2e6ccc
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-05-14 22:14:32 +0300

    add homework #12

---
+++ src/main/java/org/example/homework12/Student.java
@@ -0,0 +1,34 @@
+package org.example.homework12;
+
+public class Student {
+    String name;
+    int age;
+    int grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public Student(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade=" + grade +
+                '}';
+    }
+}

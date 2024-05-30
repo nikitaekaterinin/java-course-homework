c579a64acca74914370a76ee045aea2863e6b442
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-05-30 23:24:44 +0300

    fixed homework 13

--- src/test/java/org/example/homework13/BlackJackTests/CardTest.java
+++ src/test/java/org/example/homework13/BlackJackTests/CardTest.java
@@ -4,7 +4,6 @@ import org.example.homework13.Card;
 import org.example.homework13.Rank;
 import org.example.homework13.Suit;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,7 +12,6 @@ class CardTest {
 
     @Test
     @DisplayName("Check getting card suit")
-    @Tag("CardClassTest")
     void getSuit() {
         Card card = new Card(Suit.HEARTS, Rank.QUEEN);
 
@@ -22,7 +20,6 @@ class CardTest {
 
     @Test
     @DisplayName("Check getting card rank")
-    @Tag("CardClassTest")
     void getRank() {
         Card card = new Card(Suit.HEARTS, Rank.QUEEN);
 
@@ -31,7 +28,6 @@ class CardTest {
 
     @Test
     @DisplayName("Check Card class toString method")
-    @Tag("CardClassTest")
     void testToString() {
         Card card = new Card(Suit.HEARTS, Rank.KING);
 
